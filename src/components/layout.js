dfa504b03cf97814d2475500d4c6ad45471a396b
Manuel <mninahuanca4@gmail.com>
2021-04-29 15:56:34 -0400

    Agregando Helmet al proyecto

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import Header from './header';
+import Helmet from 'react-helmet';
 
 const Layout = (props) => {
     return (
         <>
+            <Helmet>
+                <title>Hotel Gatsby</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossOrigin="anonymous" />
+                <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@300;400;700&display=swap" rel="stylesheet"/>
+            </Helmet>
             <Header />
             {props.children}
         </>
