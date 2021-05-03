4c13c16b8aa1c69bbe2746a103fb0a5442390d42
Manuel <mninahuanca4@gmail.com>
2021-05-03 15:14:52 -0400

    Consultando la pagina de inicio con gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import * as React from "react"
 import Layout from '../components/layout';
 import ImagenHotel from '../components/imagenHotel';
+import ContenidoInicio from '../components/contenidoInicio';
+
 
 const IndexPage = () => {
   return (
     <Layout>
       <ImagenHotel />
+      <ContenidoInicio />
     </Layout>
   );
 };
